4f65e9fe93f28c8b989571fdfcc1978ad6027587
Christoph Lindstädt <christophlindstaedt@web.de>
2021-05-29 19:35:17 +0200

    added append and prepend functionality

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -5,4 +5,33 @@ export default class LinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    //if there is no head, the list is empty and the new node will be head and tail
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    //if there is a head tell the current tail that there is a new node and then set the new tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
 }
--- linked-list/__test__/LinkedList.test.js
+++ linked-list/__test__/LinkedList.test.js
@@ -7,4 +7,35 @@ describe("LinkedList", () => {
     expect(linkedList.head).toBeNull();
     expect(linkedList.tail).toBeNull();
   });
+
+  it("should append a new node", () => {
+    const linkedList = new LinkedList();
+    linkedList.append(1);
+
+    expect(linkedList.head.value).toBe(1);
+    expect(linkedList.tail.value).toBe(1);
+    expect(linkedList.head.next).toBeNull();
+    expect(linkedList.tail.next).toBeNull();
+
+    linkedList.append(2);
+
+    expect(linkedList.head.value).toBe(1);
+    expect(linkedList.tail.value).toBe(2);
+    expect(linkedList.head.next.value).toBe(2);
+  });
+
+  it("should prepend a new node", () => {
+    const linkedList = new LinkedList();
+    linkedList.prepend(1);
+
+    expect(linkedList.head.value).toBe(1);
+    expect(linkedList.tail.value).toBe(1);
+    expect(linkedList.head.next).toBeNull();
+
+    linkedList.prepend(2);
+
+    expect(linkedList.head.value).toBe(2);
+    expect(linkedList.tail.value).toBe(1);
+    expect(linkedList.head.next.value).toBe(1);
+  });
 });
