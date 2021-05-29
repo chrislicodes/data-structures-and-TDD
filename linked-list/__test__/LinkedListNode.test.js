730d6761288679b619fc5185c8a3a1ab973dd4b9
Christoph Lindstädt <christophlindstaedt@web.de>
2021-05-29 18:59:38 +0200

    Linking Test

--- linked-list/__test__/LinkedListNode.test.js
+++ linked-list/__test__/LinkedListNode.test.js
@@ -7,4 +7,15 @@ describe("LinkedListNode", () => {
     expect(node.value).toBe(1);
     expect(node.next).toBeNull();
   });
+
+  it("should link two nodes together", () => {
+    const node2 = new LinkedListNode(2);
+    const node1 = new LinkedListNode(1, node2);
+
+    expect(node1.value).toBe(1);
+    expect(node1.next).toBeDefined();
+
+    expect(node1.next.value).toBe(2);
+    expect(node2.next).toBeNull();
+  });
 });
